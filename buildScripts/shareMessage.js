b4e531129b97e97928f09d25a412a88544a8d6d9
David Madar <davidm@humaneyes.com>
2017-09-06 18:49:24 +0300

    Going much forward

--- buildScripts/shareMessage.js
+++ buildScripts/shareMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting and sharing the app in dev mode...'));
+console.log(chalk.green('Starting and sharing the app in dev mode...'));    // eslint-disable-line no-console
