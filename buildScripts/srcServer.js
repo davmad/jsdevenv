3ff1f71b7bf9d7378d9058cd8ba9031148cea1c8
David Madar <davidm@humaneyes.com>
2017-09-07 18:44:11 +0300

    Doing some mock API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,17 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+	// Hard coding for simplicity. Pretend this hits a real database
+    res.json([
+        {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+        {"id": 2, "firstName": "Sean", "lastName": "Marker", "email": "sean@gmail.com"},
+        {"id": 3, "firstName": "Addel", "lastName": "Parker", "email": "addel@gmail.com"},
+        {"id": 4, "firstName": "Manny", "lastName": "Levin", "email": "manlev@gmail.com"},
+        {"id": 5, "firstName": "Lillya", "lastName": "Laguerta", "email": "lily@gmail.com"}
+	]);
+});
+
 app.listen(port, function (err) {
 	if (err) {
 		console.log(err);
