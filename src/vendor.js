17cf07cc9528bb992e0a9cecd266668089b03652
David Gabriel Madar <davmad@secseek.com>
2017-09-09 14:40:29 +0300

    Production environment ready

---
+++ src/vendor.js
@@ -0,0 +1,10 @@
+/* This file contains references to the vendor libraries
+we're using in this project. This is used by webpack
+in the production build only. A seperate bundle for vendor
+code is useful since it's unlikely to change as often
+as the application's code. So all the ...
+ */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
---
+++ webpack.config.prod.js
@@ -0,0 +1,69 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo:false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Hash the files using MD5 so that names change when the content changes.
+    new webpackMd5Hash(),
+    // Use CommonChunkPlugin to create a seperate bundle
+    // of vendor libraries so that they're cached seperatly.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    // Create HTML file that includes reference to bundles JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+      gaToken: 'gsdgf35t4e3r5gt35t4e5tg35'
+    }),
+    // Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }, {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract(['css?sourceMap'])
+      }
+    ]
+  }
+}
